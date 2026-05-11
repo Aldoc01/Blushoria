ef05b9fe0b150394fdec6389126cc11070187b11
Aldoc01 <167122803+Aldoc01@users.noreply.github.com>
2026-05-11 07:02:41 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,31 @@
 // Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyABLuiEyrf8BvuC2hjwimhoqHYnK8ZxKRs",
-  authDomain: "blushoriastore.firebaseapp.com",
-  databaseURL: "https://blushoriastore-default-rtdb.firebaseio.com",
-  projectId: "blushoriastore",
-  storageBucket: "blushoriastore.firebasestorage.app",
-  messagingSenderId: "574652420119",
-  appId: "1:574652420119:web:3157682a34e9e942a40c52",
-  measurementId: "G-38Q74Q5SXN"
+
+apiKey:
+"AIzaSyABLuiEyrf8BvuC2hjwimhoqHYnK8ZxKRs",
+
+authDomain:
+"blushoriastore.firebaseapp.com",
+
+databaseURL:
+"https://blushoriastore-default-rtdb.firebaseio.com",
+
+projectId:
+"blushoriastore",
+
+storageBucket:
+"blushoriastore.firebasestorage.app",
+
+messagingSenderId:
+"574652420119",
+
+appId:
+"1:574652420119:web:3157682a34e9e942a40c52",
+
+measurementId:
+"G-38Q74Q5SXN"
+
 };
 
 firebase.initializeApp(firebaseConfig);
