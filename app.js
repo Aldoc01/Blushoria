f458fe5030e27d5e0f026bb56e0629d14034cc52
Aldoc01 <167122803+Aldoc01@users.noreply.github.com>
2026-05-11 13:49:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let cart = [];
 let total = 0;
 
-function addToCart(product, price){
+function addToCart(name, price){
 
-cart.push({product, price});
+cart.push({
+name,
+price
+});
 
 total += price;
 
@@ -13,8 +16,9 @@ updateCart();
 
 function updateCart(){
 
-const cartItems =
-document.getElementById("cart-items");
+const cartItems = document.getElementById("cart-items");
+
+const cartTotal = document.getElementById("cart-total");
 
 cartItems.innerHTML = "";
 
@@ -22,25 +26,48 @@ cart.forEach(item => {
 
 cartItems.innerHTML += `
 <div class="cart-item">
-${item.product} - ₦${item.price}
+${item.name} - ₦${item.price}
 </div>
 `;
 
 });
 
-document.getElementById("cart-total")
-.innerText = total;
+cartTotal.innerText = total;
 
-let message = "Hello BLUSHORIA ✨%0A%0AI want to order:%0A";
+const customerName =
+document.getElementById("customer-name").value;
+
+const customerPhone =
+document.getElementById("customer-phone").value;
+
+let message =
+"Hello Blushoria ✨%0A%0A";
+
+message += "Customer Name: " + customerName + "%0A";
+
+message += "Phone Number: " + customerPhone + "%0A%0A";
+
+message += "I want to order:%0A";
 
 cart.forEach(item => {
-message += `- ${item.product} (₦${item.price})%0A`;
+
+message += `- ${item.name} (₦${item.price})%0A`;
+
 });
 
-message += `%0ATotal: ₦${total}`;
+message += `%0ATotal: ₦${total}%0A%0A`;
 
-document.getElementById("whatsapp-order")
-.href =
-"https://wa.me/234XXXXXXXXXX?text=" + message;
+message += "Waybill delivery fee will be discussed.";
+
+document.getElementById(
+"whatsapp-order"
+).href =
+"https://wa.me/2347012620748?text=" + message;
 
 }
+
+document.getElementById("customer-name")
+.addEventListener("input", updateCart);
+
+document.getElementById("customer-phone")
+.addEventListener("input", updateCart);
