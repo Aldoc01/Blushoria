17e3bd36049ee219d4639e6de29eadabb6dad21c
Aldoc01 <167122803+Aldoc01@users.noreply.github.com>
2026-05-11 06:25:10 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,137 @@
+const ADMIN_PASSWORD = "blushoriaadmin";
+
+function loginAdmin(){
+
+const password =
+document.getElementById(
+"adminPassword"
+).value;
+
+if(password === ADMIN_PASSWORD){
+
+document.getElementById(
+"adminPanel"
+).style.display = "block";
+
+loadProducts();
+
+}else{
+
+alert("Wrong Password");
+
+}
+
+}
+
 function addProduct(){
 
-alert(
-"Firebase automatic upload coming next."
+const product = {
+
+name:
+document.getElementById(
+"productName"
+).value,
+
+price:Number(
+document.getElementById(
+"productPrice"
+).value
+),
+
+category:
+document.getElementById(
+"productCategory"
+).value,
+
+description:
+document.getElementById(
+"productDescription"
+).value,
+
+badge:
+document.getElementById(
+"productBadge"
+).value,
+
+stock:
+document.getElementById(
+"productStock"
+).value,
+
+image:
+document.getElementById(
+"productImage"
+).value
+
+};
+
+db.collection("products")
+.add(product)
+
+.then(()=>{
+
+alert("Product Added");
+
+loadProducts();
+
+});
+
+}
+
+function loadProducts(){
+
+const container =
+document.getElementById(
+"adminProducts"
 );
+
+container.innerHTML = "";
+
+db.collection("products")
+.get()
+
+.then(snapshot=>{
+
+snapshot.forEach(doc=>{
+
+const product = doc.data();
+
+container.innerHTML += `
+
+<div class="review-card">
+
+<h3>${product.name}</h3>
+
+<p>₦${product.price}</p>
+
+<p>${product.category}</p>
+
+<button onclick="deleteProduct('${doc.id}')">
+
+Delete
+
+</button>
+
+</div>
+
+`;
+
+});
+
+});
+
+}
+
+function deleteProduct(id){
+
+db.collection("products")
+.doc(id)
+.delete()
+
+.then(()=>{
+
+loadProducts();
+
+});
 
 }
