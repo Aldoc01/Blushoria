d04f664af14a0ac6d01b0ecbf3d129c54b008eb4
Aldoc01 <167122803+Aldoc01@users.noreply.github.com>
2026-05-11 04:02:32 -0700

    Update products.js

--- images/products.js
+++ images/products.js
@@ -1,83 +1,72 @@
 const products = [
   {
     id: 1,
-    name: "Lip Masks",
-    price: 400,
-    image: "images/lip-masks.jpg",
-    category: "Lip Care"
+    name: "Luxury Fashion Item",
+    price: 120,
+    image: "images/IMG-20260509-WA0021.jpg",
+    category: "Fashion"
   },
-
   {
     id: 2,
-    name: "Sadoer Face Mask Sheet",
-    price: 400,
-    image: "images/sadoer-face-mask-sheet.jpg",
-    category: "Face Mask"
+    name: "Premium Outfit",
+    price: 150,
+    image: "images/IMG-20260509-WA0034.jpg",
+    category: "Fashion"
   },
-
   {
     id: 3,
-    name: "Sadoer Salicylic Acid Face Mask",
-    price: 400,
-    image: "images/sadoer-salicylic-acid.jpg",
-    category: "Face Mask"
+    name: "Elegant Style",
+    price: 180,
+    image: "images/IMG-20260509-WA0035.jpg",
+    category: "Fashion"
   },
-
   {
     id: 4,
-    name: "Sadoer Face Masks",
-    price: 400,
-    image: "images/sadoer-face-masks.jpg",
-    category: "Face Mask"
+    name: "Modern Collection",
+    price: 210,
+    image: "images/IMG-20260509-WA0036.jpg",
+    category: "Fashion"
   },
-
   {
     id: 5,
-    name: "Cute Lipgloss",
-    price: 1200,
-    image: "images/cute-lipgloss.jpg",
-    category: "Lip Gloss"
+    name: "Classic Wear",
+    price: 130,
+    image: "images/IMG-20260509-WA0041.jpg",
+    category: "Fashion"
   },
-
   {
     id: 6,
-    name: "Clear Lip Gloss With Mirror",
-    price: 1500,
-    image: "images/clear-lipgloss-mirror.jpg",
-    category: "Lip Gloss"
+    name: "Exclusive Design",
+    price: 250,
+    image: "images/IMG-20260509-WA0043.jpg",
+    category: "Fashion"
   },
-
   {
     id: 7,
-    name: "Coloured Lipgloss",
-    price: 1500,
-    image: "images/coloured-lipgloss.jpg",
-    category: "Lip Gloss"
+    name: "Luxury Piece",
+    price: 300,
+    image: "images/IMG-20260509-WA0044.jpg",
+    category: "Fashion"
   },
-
   {
     id: 8,
-    name: "Mini Lipgloss",
-    price: 1000,
-    image: "images/mini-lipgloss.jpg",
-    category: "Lip Gloss"
+    name: "Stylish Outfit",
+    price: 175,
+    image: "images/IMG-20260510-WA0003.jpg",
+    category: "Fashion"
   },
-
   {
     id: 9,
-    name: "Magic Lipgloss",
-    price: 1200,
-    image: "images/magic-lipgloss.jpg",
-    category: "Lip Gloss"
+    name: "Premium Collection",
+    price: 220,
+    image: "images/IMG-20260510-WA0004.jpg",
+    category: "Fashion"
   },
-
   {
     id: 10,
-    name: "Magic Lipgloss With Mirror",
-    price: 1500,
-    image: "images/magic-lipgloss-mirror.jpg",
-    category: "Lip Gloss"
+    name: "Signature Fashion",
+    price: 270,
+    image: "images/IMG-20260510-WA0005.jpg",
+    category: "Fashion"
   }
 ];
-
-export default products;
