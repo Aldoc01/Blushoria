f81207a9a1940cdff25f54588cc923b1ca862a1b
Aldoc01 <167122803+Aldoc01@users.noreply.github.com>
2026-05-11 03:48:11 -0700

    Create images/products.js

---
+++ images/products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    id: 1,
+    name: "Lip Masks",
+    price: 400,
+    image: "images/lip-masks.jpg",
+    category: "Lip Care"
+  },
+
+  {
+    id: 2,
+    name: "Sadoer Face Mask Sheet",
+    price: 400,
+    image: "images/sadoer-face-mask-sheet.jpg",
+    category: "Face Mask"
+  },
+
+  {
+    id: 3,
+    name: "Sadoer Salicylic Acid Face Mask",
+    price: 400,
+    image: "images/sadoer-salicylic-acid.jpg",
+    category: "Face Mask"
+  },
+
+  {
+    id: 4,
+    name: "Sadoer Face Masks",
+    price: 400,
+    image: "images/sadoer-face-masks.jpg",
+    category: "Face Mask"
+  },
+
+  {
+    id: 5,
+    name: "Cute Lipgloss",
+    price: 1200,
+    image: "images/cute-lipgloss.jpg",
+    category: "Lip Gloss"
+  },
+
+  {
+    id: 6,
+    name: "Clear Lip Gloss With Mirror",
+    price: 1500,
+    image: "images/clear-lipgloss-mirror.jpg",
+    category: "Lip Gloss"
+  },
+
+  {
+    id: 7,
+    name: "Coloured Lipgloss",
+    price: 1500,
+    image: "images/coloured-lipgloss.jpg",
+    category: "Lip Gloss"
+  },
+
+  {
+    id: 8,
+    name: "Mini Lipgloss",
+    price: 1000,
+    image: "images/mini-lipgloss.jpg",
+    category: "Lip Gloss"
+  },
+
+  {
+    id: 9,
+    name: "Magic Lipgloss",
+    price: 1200,
+    image: "images/magic-lipgloss.jpg",
+    category: "Lip Gloss"
+  },
+
+  {
+    id: 10,
+    name: "Magic Lipgloss With Mirror",
+    price: 1500,
+    image: "images/magic-lipgloss-mirror.jpg",
+    category: "Lip Gloss"
+  }
+];
+
+export default products;
