d2ed236ef3cdd32e2597c1f8078ce035e55f7175
Aldoc01 <167122803+Aldoc01@users.noreply.github.com>
2026-05-11 16:20:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,36 +3,57 @@ let total = 0;
 
 function addToCart(name, price){
 
-    cart.push({name, price});
-    total += price;
+cart.push({
+name,
+price
+});
 
-    document.getElementById("total").innerText = `₦${total.toLocaleString()}`;
+total += price;
+
+document.getElementById("total").innerText =
+`₦${total.toLocaleString()}`;
+
 }
 
 function orderWhatsApp(){
 
-    const name = document.getElementById("name").value;
-    const address = document.getElementById("address").value;
+const customerName =
+document.getElementById("name").value;
 
-    if(cart.length === 0){
-        alert("Your cart is empty");
-        return;
-    }
+const address =
+document.getElementById("address").value;
 
-    let message = `✨ BLUSHORIA ORDER ✨%0A%0A`;
+if(cart.length === 0){
+alert("Your cart is empty");
+return;
+}
 
-    message += `👩 Name: ${name}%0A`;
-    message += `📍 Address: ${address}%0A%0A`;
+let message =
+`✨ BLUSHORIA ORDER ✨%0A%0A`;
 
-    message += `🛍️ ITEMS:%0A`;
+message +=
+`👩 Name: ${customerName}%0A`;
 
-    cart.forEach(item=>{
-        message += `• ${item.name} - ₦${item.price}%0A`;
-    });
+message +=
+`📍 Address: ${address}%0A%0A`;
 
-    message += `%0A💰 TOTAL: ₦${total.toLocaleString()}`;
+message +=
+`🛍️ ITEMS:%0A`;
 
-    const phone = "2347012620748";
+cart.forEach(item => {
 
-    window.open(`https://wa.me/${phone}?text=${message}`);
+message +=
+`• ${item.name} - ₦${item.price}%0A`;
+
+});
+
+message +=
+`%0A💰 TOTAL: ₦${total.toLocaleString()}`;
+
+const phone = "2347012620748";
+
+window.open(
+`https://wa.me/${phone}?text=${message}`
+);
+
 }
